994538f0582d6e65db82caaea5c9db99f5ee2324
poojasharadshelke <113903221+poojasharadshelke@users.noreply.github.com>
2022-09-19 16:49:13 +0530

    Add files via upload

---
+++ ams2/demo/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$dbname = "airline2";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if(mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+?>
